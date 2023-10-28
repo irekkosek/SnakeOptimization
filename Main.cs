14811ef6f15749f195f7f0564774576b94bb8224
pardonmyfriend <eva.kedziera@gmail.com>
2023-10-28 15:45:55 +0200

    Poprawka funkcji testowych

--- Main.cs
+++ Main.cs
@@ -12,78 +12,56 @@ namespace SnakeOptimization
         {
 
             double sum = 0;
-            int D = X.Length;
-            for (int i = 0; i < D; i++)
-            {
-                double xi = X[i];
-                sum += xi * xi - 10 * Math.Cos(2 * Math.PI * xi);
-            }
+            for (int i = 0; i < X.Length; i++)
+                sum += X[i] * X[i] - 10 * Math.Cos(2 * Math.PI * X[i]);
 
-            return 10 * D + sum;
+            return 10 * X.Length + sum;
 
-        }
-
-        public static double sphereFunction(params double[] X)
-        {
-
-            double sum = 0;
-            int D = X.Length;
-            for (int i = 0; i < D; i++)
-            {
-                double xi = X[i];
-                sum += xi * xi;
-            }
-
-            return sum;
         }
 
         public static double rosenbrockFunction(params double[] X)
         {
 
             double sum = 0;
-            int D = X.Length;
-            for (int i = 0; i < D - 1; i++)
-            {
-
-                double xi = X[i];
-                double xOneMore = X[i + 1];
-                sum += 100 * Math.Pow(xOneMore - xi * xi, 2) + Math.Pow(1 - xi, 2);
-            }
+            for (int i = 0; i < X.Length - 1; i++)
+                sum += 100 * Math.Pow(X[i + 1] - X[i] * X[i], 2) + Math.Pow(1 - X[i], 2);
 
             return sum;
+        }
 
+        public static double sphereFunction(params double[] X)
+        {
+
+            double sum = 0;
+            for (int i = 0; i < X.Length; i++)
+                sum += X[i] * X[i];
+
+            return sum;
         }
 
         public static double bealeFunction(params double[] X)
         {
-
             double x = X[0];
             double y = X[1];
 
             return Math.Pow(1.5 - x + x * y, 2) + Math.Pow(2.25 - x + x * y * y, 2) + Math.Pow(2.625 - x + x * y * y * y, 2);
-
         }
 
         public static double bukinFunctionN6(params double[] X)
         {
-
             double x = X[0];
             double y = X[1];
 
             return 100 * Math.Sqrt(Math.Abs(y - 0.01 * x * x)) + 0.01 * Math.Abs(x + 10);
-
         }
 
 
         public static double himmelblauFunctionN6(params double[] X)
         {
-
             double x = X[0];
             double y = X[1];
 
-
             return Math.Pow(x * x + y - 11, 2) + Math.Pow(x + y * y - 7, 2);
-
         }
     }
 }
