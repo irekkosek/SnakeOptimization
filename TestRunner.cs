40674861278609499fb7195d65c3df6e5e8e11f2
Irek Kosek <irekkosek.IK@gmail.com>
2023-10-29 18:41:47 +0100

    picking best

--- TestRunner.cs
+++ TestRunner.cs
@@ -16,7 +16,7 @@ namespace SnakeOptimization
             string reportFilePath = "raport.csv";
 
             List<TestResult> testResults = new List<TestResult>();
-
+            List<TestResult> allTestResults = new List<TestResult>();
             foreach (var testFunction in TestingFunctions.Functions)
             {
                 foreach (var nValue in Main.N)
@@ -31,7 +31,12 @@ namespace SnakeOptimization
                             _xmin: testFunction.Xmin,
                             _xmax: testFunction.Xmax
                         );
-
+                        List<TestResult> LocalIterationsResults = new List<TestResult>();
+                        double topBestFitValue=0 ;
+                        int topBestFitValueIndex=0;
+                        double worstBestFitValue=0;
+                        int worstBestFitValueIndex=0;
+                        bool first_pass = true;
                         for (int i = 0; i < Main.n; i++)
                         {
                             double[] food_position;
@@ -40,28 +45,52 @@ namespace SnakeOptimization
 
                             (food_position, bestFitValue, iFobj) = snakeOptimization.Solve(); //TODO: add {Xfood, fval, gbest, vbest, iFobj}
                             //to returned values
-
+                            //if topBestfitValue unusigned then assign it to bestFitValue
+                            if (first_pass)
+                            {
+                                topBestFitValue = bestFitValue;
+                                topBestFitValueIndex = i;
+                                worstBestFitValue = bestFitValue;
+                                worstBestFitValueIndex = i;
+                                first_pass = false;
+                            }
                             TestResult testResult = new TestResult
                             {
+                                AlgorithmName = "SnakeOptimization",
                                 TestFunctionName = testFunction.Funkcja.Method.Name,
                                 NumberOfParameters = testFunction.Dim,
-                                NumberOfIterations = tValue,
+                                NumberOfIterations = tValue, //or iFobj?
                                 PopulationSize = nValue,
-                                FoundMinimum = bestFitValue,
+                                FoundMinimum = food_position,
                                 StdDevParameters = 0, // TODO odchylenie standardowe parametrów
-                                ObjectiveValue = 0, // TODO wartość funkcji celu
+                                ObjectiveValue = bestFitValue, //wartość funkcji celu
                                 StdDevObjectiveValue = 0, // TODO odchylenie standardowe wartości funkcji celu
                             };
 
+
                             // wynik na liście
-                            testResults.Add(testResult);
+                            LocalIterationsResults.Add(testResult);
+                            allTestResults.Add(testResult);
+                            if (bestFitValue > topBestFitValue)
+                            {
+                                topBestFitValue = bestFitValue;
+                                topBestFitValueIndex = i;
+                            }
+                            if (bestFitValue < worstBestFitValue)
+                            {
+                                worstBestFitValue = bestFitValue;
+                                worstBestFitValueIndex = i;
+                            }
+
+
                             //log formatted result to stdout
                             Console.WriteLine($"TestFunctionName: {testResult.TestFunctionName}, NumberOfParameters: {testResult.NumberOfParameters}, NumberOfIterations: {testResult.NumberOfIterations}, PopulationSize: {testResult.PopulationSize}, FoundMinimum: {testResult.FoundMinimum}, StdDevParameters: {testResult.StdDevParameters}, ObjectiveValue: {testResult.ObjectiveValue}, StdDevObjectiveValue: {testResult.StdDevObjectiveValue}");
-                            //TODO: add logic that will ~~gather all the data from each~~ run and then
-                            //decide which one is the best and the worst (based on fval)
-                            //data can be easily gathered using method inside the SnakeOptimization class that gathers needed information:
-                            //result=snakeOptimization.Result()
+
+      
                         }
+                            // assign best and worst results to testResult
+                            testResults.Add(LocalIterationsResults[topBestFitValueIndex]);
+                            testResults.Add(LocalIterationsResults[worstBestFitValueIndex]);
                     }
                 }
             }
@@ -73,6 +102,11 @@ namespace SnakeOptimization
             {
                 csv.WriteRecords(testResults); 
                 // based on table from the assignment (see https://platforma.polsl.pl/rms/pluginfile.php/235440/mod_resource/content/1/Heurystyki___instrukcja_do_test%C3%B3w.pdf)
+            }
+            using (var writer = new StreamWriter("all"+reportFilePath))
+            using (var csv = new CsvWriter(writer, config))
+            {
+                csv.WriteRecords(allTestResults); 
             }
         }
     }
